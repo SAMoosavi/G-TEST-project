5e3d5265d141072e8a28ccdea2a9c0ded1f7d640
moosavi <moosavi238@gmail.com>
2022-10-10 10:06:03 +0330

    complete HelperFunctions.h

--- include/HelperFunctions.h
+++ include/HelperFunctions.h
@@ -5,4 +5,19 @@
 #ifndef G_TEST_MATRIX_HELPERFUNCTIONS_H
 #define G_TEST_MATRIX_HELPERFUNCTIONS_H
 
+class HelperFunctions {
+public:
+    template<typename T>
+    static T **createMatrix(int row, int col);
+
+    template<typename T>
+    static T **createMatrix(int row, int col, T type);
+
+    template<class T>
+    static void deleteMatrix(int row, T **&matrix);
+
+private:
+    HelperFunctions() = default;
+};
+
 #endif //G_TEST_MATRIX_HELPERFUNCTIONS_H
