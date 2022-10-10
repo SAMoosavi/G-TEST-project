17b072e666374dc8e0105d3b88f10edb9f521156
moosavi <moosavi238@gmail.com>
2022-10-10 10:11:40 +0330

    complete MatrixError.h

--- include/MatrixError.h
+++ include/MatrixError.h
@@ -5,4 +5,23 @@
 #ifndef G_TEST_MATRIX_MATRIXERROR_H
 #define G_TEST_MATRIX_MATRIXERROR_H
 
+#include <string>
+
+using std::string;
+
+class MatrixError : public std::exception {
+public:
+    string getError() {
+        return errorMsg;
+    }
+
+protected:
+    string errorMsg;
+};
+
+class SizeError : public MatrixError {
+private:
+    string errorMsg = "not correct size";
+};
+
 #endif //G_TEST_MATRIX_MATRIXERROR_H
