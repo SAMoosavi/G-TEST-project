67a18a7f2cae84404f6da73d24c710d2dee1365c
moosavi <moosavi238@gmail.com>
2022-10-10 10:07:36 +0330

    complete HelperFunctions.cpp

--- src/HelperFunctions.cpp
+++ src/HelperFunctions.cpp
@@ -1,3 +1,30 @@
 //
 // Created by moosavi on 10/10/22.
 //
+
+#include "../include/HelperFunctions.h"
+
+template<typename T>
+T **HelperFunctions::createMatrix(int row, int col) {
+    if (row <= 0 || col <= 0)
+        throw "size not validation";
+    T **matrix = new T *[row];
+    for (int i = 0; i < row; ++i) {
+        matrix[i] = new T[col];
+    }
+    return matrix;
+}
+
+template<typename T>
+T **HelperFunctions::createMatrix(int row, int col, T type) {
+    return HelperFunctions::createMatrix<T>(row, col);
+}
+
+template<typename T>
+void HelperFunctions::deleteMatrix(int row, T **&matrix) {
+    for (int i = 0; i < row; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+    matrix = nullptr;
+}
