cffb87f3d3a124f5ce684ed3fdf16f1548adb32d
moosavi <moosavi238@gmail.com>
2022-10-11 17:14:42 +0330

    complete TODO in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,5 @@
 #include "include/HelperFunctions.h"
 
 int main() {
-    auto a = HelperFunctions::createMatrix<int>(3, 3); // TODO: find error: undefined reference to `T** HelperFunctions::createMatrix<T>(int, int)'
+    auto a = HelperFunctions::createMatrix<int>(3, 3);
 }
